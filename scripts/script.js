06f59d2d30f6e02ea867515027f9d7fc2819640f
balajich004 <balajichennupati123@gmail.com>
2024-06-13 22:31:18 +0530

    done some styling to the game page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,38 @@
-const player1=document.querySelector("#player1");
-const player2=document.querySelector("#player2");
+function handlePlayerDetails(){
+    const player1=document.querySelector("#player1");
+    const player2=document.querySelector("#player2");
+    const submit=document.querySelector(".submit");
+    if (submit && player1 && player2) {
+        submit.addEventListener("click",(event)=>{
+            event.preventDefault();
+            localStorage.setItem("player1",player1.value);
+            localStorage.setItem("player2",player2.value);
+            window.location.href = "./playersgame.html";
+        });
+    }
+}
+function display() {
+    const player1_title=document.querySelector(".player1-title");
+    const player2_title=document.querySelector(".player2-title");
+    if (player1_title && player2_title) {
+        const player1Name=localStorage.getItem("player1");
+        const player2Name=localStorage.getItem("player2");
+        if(player1_title && player2_title){
+            player1_title.innerText = player1Name+" 🥷";
+            player2_title.innerText = "🥷 "+player2Name;
+        }
+        else{
+            player1_title="Player 1";
+            player2_title="Player 2";
+        }
+    }
+}
+let cell=document.querySelector(".cell");
+cell.addEventListener("click",()=>{
+    cell.innerText="X";
+});
 
+document.addEventListener("DOMContentLoaded",()=>{
+    handlePlayerDetails();
+    display();
+});
